db601ee7217431acc111806f17084ab3034ec48d
Triggered41 <vehdathamid@gmail.com>
2021-06-05 12:18:49 +0530

    both the lablels in main menu and game scene now shares the same font resource

--- Start_Menu.gd
+++ Start_Menu.gd
@@ -1,12 +1,6 @@
 extends CanvasLayer
 
-
 onready var high_score = $Label
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	high_score.text = "High Score: " + str(SaveSystem.data["high_score"])
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
