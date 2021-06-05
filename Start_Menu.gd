0ae79b7913ed0118b1801b1a4cd5dae759e733bb
Triggered41 <vehdathamid@gmail.com>
2021-06-05 11:54:49 +0530

    typo mistake

--- Start_Menu.gd
+++ Start_Menu.gd
@@ -4,7 +4,7 @@ extends CanvasLayer
 onready var high_score = $Label
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	high_score.text = "Score: " + str(SaveSystem.data["high_score"])
+	high_score.text = "High Score: " + str(SaveSystem.data["high_score"])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
